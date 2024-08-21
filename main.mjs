19531940e12cf0768d758023efb8b4e4ff150567
palmwi2010 <wop3000@hotmail.co.uk>
2024-08-21 18:12:22 +0100

    Added Odin Project test

---
+++ main.mjs
@@ -0,0 +1,23 @@
+import HashMap from "./hash.mjs";
+
+const test = new HashMap();
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+test.set('moon', 'silver')
+
+test.set('lion', 'brown');
+
+console.log(test.entries());
+console.log(test.length());
+console.log(test.capacity);
